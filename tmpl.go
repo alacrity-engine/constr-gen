ae8f95549f0a447bd94f58482c58f0e1803cf80d
zergon321 <maximgradan@gmail.com>
2023-10-10 17:10:33 +0300

    Fixes.

--- tmpl.go
+++ tmpl.go
@@ -2,9 +2,15 @@ package main
 
 func getCompPkgSet(comps []ComponentData) map[string]int {
 	compPkgs := map[string]int{}
+	count := 0
 
-	for i, comp := range comps {
-		compPkgs["\""+comp.PkgPath+"\""] = i
+	for _, comp := range comps {
+		compPkg := "\"" + comp.PkgPath + "\""
+
+		if _, ok := compPkgs[compPkg]; !ok {
+			compPkgs[compPkg] = count
+			count++
+		}
 	}
 
 	return compPkgs
